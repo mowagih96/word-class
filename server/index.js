331f52b92905c33d0d52a6889ce6f07d8c9536f6
Mohamed Wagih <mowagih96.dev@outlook.com>
2022-12-22 21:16:25 +0200

    Connect the client to the server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 // Start express app
 const app = express();
 
+// Serve React app
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
 // React to requests
 // Route handling
-app.get('/', (req, res) => {
-  res.send('<p>Hello World!</p>');
+app.get('/words', (req, res) => {
+  res.send({ msg: 'hello' });
 });
 
 // Listen for requests
