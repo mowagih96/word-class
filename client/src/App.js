91cf522a2d217a12b3f226b502e93d1170ab41be
Mohamed Wagih <mowagih96.dev@outlook.com>
2022-12-24 13:41:15 +0200

    Add React Router and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Practice from './routes/Practice';
+import Rank from './routes/Rank';
+
 function App() {
-  return <div className='App'>React boi</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/practice' element={<Practice />} />
+        <Route path='/rank' element={<Rank />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
