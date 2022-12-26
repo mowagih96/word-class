a528ff43d08185a560b86546621f2f870b96baf8
Mohamed Wagih <mowagih96.dev@outlook.com>
2022-12-26 17:49:34 +0200

    Use the calculated student score in the Rank route

--- client/src/routes/Rank.js
+++ client/src/routes/Rank.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import rankStudent from '../api/rank';
 
 const Rank = () => {
   const [studentRank, setStudentRank] = useState(null);
+  const { state: studentScore } = useLocation();
 
   useEffect(() => {
     const getStudentRank = async () => {
       try {
-        const { status, data } = await rankStudent(90);
+        const { status, data } = await rankStudent(studentScore);
 
         if (status === 200) setStudentRank(data.rank);
       } catch (error) {
@@ -16,7 +18,7 @@ const Rank = () => {
     };
 
     getStudentRank();
-  }, [studentRank]);
+  }, [studentScore]);
   return <div>{studentRank ? `Student Rank: ${studentRank}` : ''}</div>;
 };
 
