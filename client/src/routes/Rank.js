ddfd5c3885a5ba79fbebcf79c2b801ee0b0f64fb
Mohamed Wagih <mowagih96.dev@outlook.com>
2022-12-26 17:51:09 +0200

    Add Try again button and Navigation logic

--- client/src/routes/Rank.js
+++ client/src/routes/Rank.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button } from '@mantine/core';
 import rankStudent from '../api/rank';
 
 const Rank = () => {
   const [studentRank, setStudentRank] = useState(null);
   const { state: studentScore } = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getStudentRank = async () => {
@@ -19,7 +21,12 @@ const Rank = () => {
 
     getStudentRank();
   }, [studentScore]);
-  return <div>{studentRank ? `Student Rank: ${studentRank}` : ''}</div>;
+  return (
+    <div>
+      {studentRank ? `Student Rank: ${studentRank}` : ''}
+      <Button onClick={() => navigate('/practice')}>Try again</Button>
+    </div>
+  );
 };
 
 export default Rank;
