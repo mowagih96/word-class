a33006f890571cee0b53c21e4e3e5537c61bfa36
Mohamed Wagih <mowagih96.dev@outlook.com>
2022-12-27 15:48:58 +0200

    Revert "Add SASS styles folder and Style Home route"
    This reverts commit c606d2554b7b3af68295635ee4fc8bab638a8b39.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Practice from './routes/Practice';
 import Rank from './routes/Rank';
-import './styles/main.scss';
 
 function App() {
   return (
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Paper, TextInput, Button } from '@mantine/core';
+import { TextInput, Button } from '@mantine/core';
 
 const Home = () => {
   const [studentName, setStudentName] = useState('');
@@ -35,23 +35,18 @@ const Home = () => {
   };
 
   return (
-    <div className='layout'>
-      <Paper shadow='sm' withBorder>
-        <form className='student-name-form' onSubmit={handleSubmit}>
-          <TextInput
-            className='student-name-form__text-input'
-            placeholder='Please enter your name'
-            label='Full name'
-            withAsterisk
-            value={studentName}
-            onChange={handleChange}
-            error={isEmpty && 'Invalid name'}
-          />
-          <Button className='student-name-form__submit-button' type='submit'>
-            Submit
-          </Button>
-        </form>
-      </Paper>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextInput
+          placeholder='Please enter your name'
+          label='Full name'
+          withAsterisk
+          value={studentName}
+          onChange={handleChange}
+          error={isEmpty && 'Invalid name'}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
     </div>
   );
 };
