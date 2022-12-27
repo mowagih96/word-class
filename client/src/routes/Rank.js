cf95aabbf41ec32e141fa914377e13b362926014
Mohamed Wagih <mowagih96.dev@outlook.com>
2022-12-27 10:51:50 +0200

    Display student name in Rank route

--- client/src/routes/Rank.js
+++ client/src/routes/Rank.js
@@ -7,18 +7,20 @@ import rankStudent from '../api/rank';
 
 const Rank = () => {
   const [studentRank, setStudentRank] = useState(null);
-  const { state: studentScore } = useLocation();
+  const {
+    state: { studentName, studentFinalScore },
+  } = useLocation();
   const navigate = useNavigate();
   const { width, height } = useWindowSize();
 
-  // On the initial mount and 'studentScore' state change:
+  // On the initial mount and 'studentFinalScore' state change:
   // - Initiate a POST request to the backend server and wait for the response with the rank.
   // - If the request succeeded update the 'studentRank' state.
   // - If the request failed log the error to the console.
   useEffect(() => {
     const getStudentRank = async () => {
       try {
-        const { status, data } = await rankStudent(studentScore);
+        const { status, data } = await rankStudent(studentFinalScore);
 
         if (status === 200) setStudentRank(data.rank);
       } catch (error) {
@@ -27,12 +29,14 @@ const Rank = () => {
     };
 
     getStudentRank();
-  }, [studentScore]);
+  }, [studentFinalScore]);
 
   return (
     <div>
       <Confetti width={width} height={height} recycle={false} />
-      {studentRank ? `Student Rank: ${studentRank}` : ''}
+      {studentRank
+        ? `Congratulations ${studentName}! Your Rank is ${studentRank}`
+        : ''}
       <Button onClick={() => navigate('/practice')}>Try again</Button>
     </div>
   );
